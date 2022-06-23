22af2521a7264d6e6f2480183e0857de4e2964f7
Enrique Delgado <edelgado@motionpoint.com>
2022-06-22 20:44:58 -0400

    Adding pagination css classes wip

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -11,6 +11,8 @@ import { fetchTasks } from "./components/tasks/tasksSlice";
 import { useEffect } from "react";
 import { useAppDispatch } from "../../app/hooks";
 
+import Pagination from "react-js-pagination";
+
 const Home = () => {
   const isExpanded = useAppSelector(selectIsExpanded);
   const dispatch = useAppDispatch();
@@ -32,6 +34,10 @@ const Home = () => {
     dispatch(fetchTasks(requestObject));
   }, []);
 
+  const handlePageChange = (page) => {
+
+  }
+
   return (
     <>
       <div style={{ marginTop: "65px" }}></div>
@@ -41,7 +47,24 @@ const Home = () => {
           <div className="mt-1" style={{ marginLeft: isExpanded ? "257px" : "57px" }}>
             <ActionBar />
             <TaskTable />
-            {/* pagination here */}
+            <div className="bg-white p-4 sm:px-6 border-2 border-gray-150 rounded mt-6">
+              <div className="flex space-x-3">
+                 <div className='tpm-pagination-container'>
+                    <Pagination
+                        innerClass="pagination pagination-custom"
+                        activePage={1}
+                        hideDisabled
+                        itemClass="page-item page-item-custom"
+                        linkClass="page-link page-link-custom"
+                        itemsCountPerPage={200}
+                        totalItemsCount={50000}
+                        pageRangeDisplayed={4}
+                        onChange={handlePageChange}
+                        />
+                  </div>
+                
+              </div>
+            </div>
             <div className="p-5"></div>
           </div>
         </div>
